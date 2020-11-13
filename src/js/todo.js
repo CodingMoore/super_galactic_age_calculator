1dbb15493560278dfc479e37e7358c9424990c39
Randel M <Randel.C.Moore@gmail.com>
2020-11-13 08:46:59 -0800

    add basic pseudo code for project comprehension/planning

---
+++ src/js/todo.js
@@ -0,0 +1,18 @@
+const { arguments } = require("file-loader")
+
+//pseudo code
+
+//input age
+//input life expectancy
+
+//class (
+// planet1
+// planet2
+// planet3)
+
+//constructor person(
+// age
+// life expectancy
+
+
+//use template literals for output statements (strings)
