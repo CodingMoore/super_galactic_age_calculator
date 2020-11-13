9692b355de9e0c2158cf4ecf43f996d7b8e0347a
Randel M <Randel.C.Moore@gmail.com>
2020-11-13 13:28:51 -0800

    pass Planet() init test

--- __tests__/Planet.test.js
+++ __tests__/Planet.test.js
@@ -15,7 +15,7 @@ describe("Planet", () => {
   });
 
   test("Should correctly create and instance of Planet() with properties 'name' and 'factor'.", () => {
-    expect(mercury).toEqual({name: "Mercury", factor: .23})
+    expect(mercury).toEqual({name: "Mercury", factor: .24})
   });
 
 });
